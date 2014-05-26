62cf30aa5263e9649308ac73453b8fbb636cc2ed
Neyllor Barbosa <neyllorbarbosa@MacBook-Pro-de-Neyllor.local>
2014-05-26 17:17:03 -0300

    Criei o primeiro XIB - Main

---
+++ jogoPalito/jogoPalito/BEPMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  BEPMainViewController.h
+//  jogoPalito
+//
+//  Created by Neyllor Barbosa on 26/05/14.
+//  Copyright (c) 2014 Luiz Ilha Moschem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BEPMainViewController : UIViewController
+
+@end
---
+++ jogoPalito/jogoPalito/BEPMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  BEPMainViewController.m
+//  jogoPalito
+//
+//  Created by Neyllor Barbosa on 26/05/14.
+//  Copyright (c) 2014 Luiz Ilha Moschem. All rights reserved.
+//
+
+#import "BEPMainViewController.h"
+
+@interface BEPMainViewController ()
+
+@end
+
+@implementation BEPMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
