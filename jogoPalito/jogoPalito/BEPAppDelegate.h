60b3da61f0c41ac3a71629294979100bd08ba532
Luiz Ilha Moschem <luizilha@Luizs-MacBook-Pro.local>
2014-05-26 16:43:07 -0300

    Inicial

---
+++ jogoPalito/jogoPalito/BEPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BEPAppDelegate.h
+//  jogoPalito
+//
+//  Created by Luiz Ilha Moschem on 5/26/14.
+//  Copyright (c) 2014 Luiz Ilha Moschem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BEPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ jogoPalito/jogoPalito/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  jogoPalito
+//
+//  Created by Luiz Ilha Moschem on 5/26/14.
+//  Copyright (c) 2014 Luiz Ilha Moschem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BEPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BEPAppDelegate class]));
+    }
+}
